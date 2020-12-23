e8a1e53daf83dc1f256cd9a24afb4564945d2e0c
Gabriel Dias Vieira <gabrieldv.dv@hotmail.com>
2020-12-22 23:03:34 -0300

    New components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
+import { ListProductComponent } from './components/list-product/list-product.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateProductComponent,
+    ListProductComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
