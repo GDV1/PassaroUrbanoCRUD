4f2b76e376a203f2dfe078cf186787c523a95354
Gabriel Dias Vieira <gabrieldv.dv@hotmail.com>
2020-12-23 12:39:38 -0300

    New template components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+/* Components */
 import { CreateProductComponent } from './components/create-product/create-product.component';
 import { ListProductComponent } from './components/list-product/list-product.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { NavbarComponent } from './template/navbar/navbar.component';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+
 
 /* Material Imports */
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +30,10 @@ import {MatButtonModule} from '@angular/material/button';
     CreateProductComponent,
     ListProductComponent,
     LoginComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -31,6 +43,7 @@ import {MatButtonModule} from '@angular/material/button';
     /*Material Modules - Start*/
     MatGridListModule,
     MatButtonModule,
+    MatToolbarModule,
     /*Material Modules - End*/
 
     AngularFireModule.initializeApp(environment.firebaseConfig)
