4f2f6f0fdbe4096ded0567e541b900c8952cd411
Gabriel Dias Vieira <gabrieldv.dv@hotmail.com>
2020-12-23 11:20:55 -0300

    Setting Angular Fire & Firebase Config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,17 @@
 
 
 export const environment = {
-  production: false 
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBgoZqfUCNq1FTCR-92KzPvPtOPpJs4EGo",
+    authDomain: "passaro-urbano-1690c.firebaseapp.com",
+    databaseURL: "https://passaro-urbano-1690c-default-rtdb.firebaseio.com",
+    projectId: "passaro-urbano-1690c",
+    storageBucket: "passaro-urbano-1690c.appspot.com",
+    messagingSenderId: "216538848031",
+    appId: "1:216538848031:web:7b83d23216e1d004f0ee2a",
+    measurementId: "G-TMC2QYKEYQ"
+  }
 };
 
 /*
