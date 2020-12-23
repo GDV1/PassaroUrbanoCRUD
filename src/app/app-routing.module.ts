b77d70d85c8da1181bf18c875ba713d983b0e1ff
Gabriel Dias Vieira <gabrieldv.dv@hotmail.com>
2020-12-23 12:16:47 -0300

    New component and new main view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateProductComponent } from './components/create-product/create-product.component';
+import { ListProductComponent } from './components/list-product/list-product.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductCrudComponent },
+  { path: 'cadastrar', component: CreateProductComponent },
+  { path: 'pesquisar', component: ListProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,33 @@ import { CreateProductComponent } from './components/create-product/create-produ
 import { ListProductComponent } from './components/list-product/list-product.component';
 import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+
+/* Material Imports */
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateProductComponent,
     ListProductComponent,
-    LoginComponent
+    LoginComponent,
+    ProductCrudComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    
+    /*Material Modules - Start*/
+    MatGridListModule,
+    MatButtonModule,
+    /*Material Modules - End*/
+
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
